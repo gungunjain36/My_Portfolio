8b620dd545d8b9089d75f17d925d7051a78605ff
gungunjain36 <jgungun1723@gmail.com>
2024-07-06 22:35:38 +0530

    Resume link changed

--- src/components/CV.jsx
+++ src/components/CV.jsx
@@ -4,7 +4,7 @@ function CV(){
     return(
         <div className="CV box">
             <div>
-            <a href="https://drive.google.com/file/d/1uNw5jVjd9GEweN16whU2QVcIW99_XNGA/view?usp=sharing"><img src={require("../images/cv.png")} alt="image" className="cv-image"/></a>
+            <a href="https://drive.google.com/file/d/13BTvoJL5n7aRaRe77vFdShcqDi8fGzro/view?usp=sharing"><img src={require("../images/cv.png")} alt="image" className="cv-image"/></a>
             </div>
             <div className="cv-link"><a href="https://drive.google.com/file/d/1uNw5jVjd9GEweN16whU2QVcIW99_XNGA/view?usp=sharing">Download CV</a></div>
         </div>
