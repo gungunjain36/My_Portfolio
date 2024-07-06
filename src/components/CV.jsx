1834f5275285061ceedb2d5ca0debb905791a9f9
gungunjain36 <jgungun1723@gmail.com>
2024-07-06 22:51:00 +0530

    link to resume updated

--- src/components/CV.jsx
+++ src/components/CV.jsx
@@ -6,7 +6,7 @@ function CV(){
             <div>
             <a href="https://drive.google.com/file/d/13BTvoJL5n7aRaRe77vFdShcqDi8fGzro/view?usp=sharing"><img src={require("../images/cv.png")} alt="image" className="cv-image"/></a>
             </div>
-            <div className="cv-link"><a href="https://drive.google.com/file/d/1uNw5jVjd9GEweN16whU2QVcIW99_XNGA/view?usp=sharing">Download CV</a></div>
+            <div className="cv-link"><a href="https://drive.google.com/file/d/13BTvoJL5n7aRaRe77vFdShcqDi8fGzro/view?usp=sharing">Download CV</a></div>
         </div>
     )
 }
